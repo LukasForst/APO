ecd9d4766ea7334f2c70ef22bf4b3e489a5a4b4c
forstluk <forstluk@fel.cvut.cz>
2017-04-04 23:58:26 +0200

    now it is working

--- hw02/main.cpp
+++ hw02/main.cpp
@@ -1,74 +1,82 @@
-#include <iostream>
-#include <string.h>
-#include <cstring>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 
-/*
- *   0  -1  0
-    -1  5   -1
-     0	-1	0
- */
+#define OUTPUT "output.ppm"
 
-int main(int argc, char* argv[]) {
-    char *fname;
-    const char *output = "output.ppm";
+int main(int argc, char *argv[]) {
 
-    if(argc > 1){
-        fname  = argv[1];
-    } else {
+    FILE *fp = argc > 1 ? fopen(argv[1], "rb") : NULL;
+    if(!fp)
         return EXIT_FAILURE;
-    }
 
-    FILE *f = fopen(fname, "rb");
-    if(!f){
-        fprintf(stderr, "WRONG FILE\n");
+    int width, height, color_max;
+    if (fscanf(fp, "P6%d %d %d", &width, &height, &color_max) == 0)
         return EXIT_FAILURE;
-    }
 
-    uint32_t height, width, max_colour, picture_width;
-    fscanf (f, "P6 %d %d %d",&picture_width, &height, &max_colour);
-    width = 3*picture_width;
+    register int current_pixel,
+            idx = 0,
+            array_width = width * 3,
+            size = array_width*height,
+            active_row = 1;
 
-    uint32_t size = width*height;
-    char *old_ppm = new char[size];
-    fread(old_ppm,size,1,f);
+    //allocate and read
+    unsigned char *old_ppm = new unsigned char[size];
+    if (fread(old_ppm, sizeof(unsigned char), (size_t) size, fp) == 0)
+        return EXIT_FAILURE;
 
-    int tmp;
+    fclose(fp);
 
-    FILE *newf = fopen(output, "wb");
-    fprintf(newf,"P6\n%d\n%d\n%d\n",picture_width, height, max_colour);
+    int gray_scale[] = {0,0,0,0,0};
+    double ratio[] = {0.2126, 0.7152, 0.0722};
 
-    register uint32_t idx = 0, w = 1;
-    for(; idx != size;){
-
-        if (w == picture_width || w == 1 || idx < width || idx >= (width*height-width)) {
-            fputc(old_ppm[idx], f);
-            fputc(old_ppm[idx+1], f);
-            fputc(old_ppm[idx+2], f);
-
-            if (w == picture_width)
-                w = 1;
-            else ++w;
-            idx += 3;
+    register int gray = 0;
+    //write image
+    FILE *new_ppm = fopen(OUTPUT, "wb");
+    fprintf(new_ppm, "P6\n%d\n%d\n%d", width, height, color_max);
+    for (;idx != size;) {
+        if (active_row == width || active_row == 1 || idx < array_width || idx >= (array_width*height-array_width)) {
+            fputc(old_ppm[idx++], new_ppm);
+            fputc(old_ppm[idx++], new_ppm);
+            fputc(old_ppm[idx++], new_ppm);
+            active_row = active_row == width ? 1 : active_row + 1;
         } else {
-            for(int i = 0; i < 3; idx++, i++){
-                tmp = 5*old_ppm[idx];
-                tmp -= old_ppm[idx - 3];
-                tmp -= old_ppm[idx + 3];
-                tmp -= old_ppm[idx - width];
-                tmp -= old_ppm[idx + width];
-                tmp = tmp > 255 ? 255 : tmp;
-                tmp = tmp < 0 ? 0 : tmp;
-                fputc(tmp, newf);
+            gray = 0;
+            for(int i = 0; i < 3; i++, idx++){
+                current_pixel = 5*old_ppm[idx];
+                current_pixel -= old_ppm[idx-3];
+                current_pixel -= old_ppm[idx-array_width];
+                current_pixel -= old_ppm[idx+3];
+                current_pixel -= old_ppm[idx+array_width];
+
+                current_pixel = current_pixel > 255 ? 255 : current_pixel;
+                current_pixel = current_pixel < 0 ? 0 : current_pixel;
+
+                //gray scale computation
+                gray += ratio[i]*current_pixel;
+
+                fputc((char)current_pixel, new_ppm);
             }
-            w++;
+            ++active_row;
+
+            if(gray < 51){
+                gray_scale[0]++;
+            } else if(gray < 102){
+                gray_scale[1]++;
+            } else if(gray < 153){
+                gray_scale[2]++;
+            } else if(gray < 204){
+                gray_scale[3]++;
+            } else {
+                gray_scale[4]++;
+            }
         }
-
     }
+    delete[] old_ppm;
+    FILE *histogram = fopen("output.txt", "wb");
+    fprintf(histogram, "%d %d %d %d %d ", gray_scale[0], gray_scale[1], gray_scale[2], gray_scale[3], gray_scale[4]);
 
-    fclose(newf);
-    fclose(f);
+    fclose(new_ppm);
+    fclose(histogram);
+
     return EXIT_SUCCESS;
 }
