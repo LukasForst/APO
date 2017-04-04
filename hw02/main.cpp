5e74e84dcd8e5be14b1a359df57874d2643c0891
forstluk <forstluk@fel.cvut.cz>
2017-04-04 22:11:13 +0200

    almost there

--- hw02/main.cpp
+++ hw02/main.cpp
@@ -36,31 +36,44 @@ int main(int argc, char* argv[]) {
     char *new_ppm = new char[size];
     fread(old_ppm,size,1,f);
 
-
     int row, column, colour, tmp;
 
+    register uint32_t idx;
+    for(idx = 0; idx < size; idx++){
+        tmp = 0;
+        tmp += 5*old_ppm[idx];
+        tmp -= idx >= 3 ? old_ppm[idx - 3] : 0;
+        tmp -= idx < size - 3 ? old_ppm[idx + 3] : 0;
+        tmp -= idx >= width  ? old_ppm[idx - width] : 0;
+        tmp -= idx < size - width ? old_ppm[idx + width] : 0;
+
+        tmp = tmp > 255 ? 255 : tmp;
+        tmp = tmp < 0 ? 0 : tmp;
+
+        new_ppm[idx] = (char) tmp;
+    }
+
+
+/*
     for(row = 0; row < height; row++){
-        for(column = 0; column < width; column += 3){
+        for(column = 0; column < width; column++){
 
             for(colour = 0; colour < 3; colour++){
-                //new_ppm[width * row + column + colour] = (-1)*old_ppm[width * (row - 1) + column + colour];
                 tmp = 0;
-                tmp
-                        += row > 0 ? (-1)*old_ppm[width * (row - 1) + column + colour] : 0;
-                tmp
-                        += column > 0 ? (-1)*old_ppm[width * row + (column - 1) + colour] : 0;
-                tmp
-                        += 5*old_ppm[width * row + column + colour];
-                tmp
-                        += column < width - 1 - colour ? (-1)*old_ppm[width * row + (column + 1) + colour] : 0;
-                tmp
-                        += row < height - 1 ? (-1)*old_ppm[width * (row + 1) + column + colour] : 0;
+                tmp -= row > 0 ? old_ppm[width * (row - 1) + column + colour] : 0;
+                tmp -= column > 0 ? old_ppm[width * row + (column - 1) + colour] : 0;
+                tmp += 5*old_ppm[width * row + column + colour];
+                tmp -= column < width - 1 - colour ? old_ppm[width * row + (column + 1) + colour] : 0;
+                tmp -= row < height - 1 ? old_ppm[width * (row + 1) + column + colour] : 0;
+
                 tmp = tmp > 255 ? 255 : tmp;
                 tmp = tmp < 0 ? 0 : tmp;
+
                 new_ppm[width * row + column + colour] = (char) tmp;
             }
         }
     }
+*/
 
 
     //write ppm
