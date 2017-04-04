f489e7f2104b1867bf4aca366a6da80d6cf26cc4
forstluk <forstluk@fel.cvut.cz>
2017-04-04 20:36:52 +0200

    read and write

--- hw02/main.cpp
+++ hw02/main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <string.h>
-
-void print(char *text){
-    std::cout << text << std::endl;
-}
+#include <cstring>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[]) {
     char *fname;
+    const char *output = "output.ppm";
 
     if(argc > 1){
         fname  = argv[1];
@@ -14,9 +14,26 @@ int main(int argc, char* argv[]) {
         return EXIT_FAILURE;
     }
 
-    FILE *f = fopen(fname, "w");
-    if(!f) return EXIT_FAILURE;
+    FILE *f = fopen(fname, "rb");
+    if(!f){
+        fprintf(stderr, "WRONG FILE\n");
+        return EXIT_FAILURE;
+    }
+
+    int height, width, max_colour;
+    fscanf (f, "P6 %d %d %d",&width, &height, &max_colour);
+
+    char *data = new char[width*height * 3];
+    fread(data,width*height * 3,1,f);
 
 
-    return 0;
+
+    //write ppm
+    FILE *newf = fopen(output, "wb");
+    fprintf(newf,"P6\n%d\n%d\n%d",width, height, max_colour);
+    fwrite(data, width * 3, height, newf);
+
+    fclose(newf);
+    fclose(f);
+    return EXIT_SUCCESS;
 }
