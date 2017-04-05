c50bbd6a5c2c54356e2750ab3ca5b142bc12c4c3
forstluk <forstluk@fel.cvut.cz>
2017-04-05 10:54:11 +0200

    some updates

--- hw02/main.cpp
+++ hw02/main.cpp
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <cmath>
 
 #define OUTPUT "output.ppm"
+#define HISTOGRAM "output.txt"
 
 int main(int argc, char *argv[]) {
-
-    FILE *fp = argc > 1 ? fopen(argv[1], "rb") : NULL;
-    if(!fp)
+    FILE *old_file = argc > 1 ? fopen(argv[1], "rb") : NULL;
+    if(!old_file)
         return EXIT_FAILURE;
 
     int width, height, color_max;
-    if (fscanf(fp, "P6%d %d %d", &width, &height, &color_max) == 0)
+    if (fscanf(old_file, "P6%d %d %d", &width, &height, &color_max) == 0)
         return EXIT_FAILURE;
 
     register int current_pixel,
@@ -20,28 +21,36 @@ int main(int argc, char *argv[]) {
             active_row = 1;
 
     //allocate and read
-    unsigned char *old_ppm = new unsigned char[size];
-    if (fread(old_ppm, sizeof(unsigned char), (size_t) size, fp) == 0)
+    unsigned char *old_ppm = (unsigned char*)malloc(sizeof(unsigned char) * size);
+    if (fread(old_ppm, sizeof(unsigned char), size, old_file) == 0)
         return EXIT_FAILURE;
-
-    fclose(fp);
+    fclose(old_file);
 
     int gray_scale[] = {0,0,0,0,0};
     double ratio[] = {0.2126, 0.7152, 0.0722};
 
-    register int gray = 0;
+    register double gray = 0;
     //write image
     FILE *new_ppm = fopen(OUTPUT, "wb");
     fprintf(new_ppm, "P6\n%d\n%d\n%d", width, height, color_max);
-    for (;idx != size;) {
+
+
+
+    for (;idx != size; gray = 0) {
         if (active_row == width || active_row == 1 || idx < array_width || idx >= (array_width*height-array_width)) {
+            gray += old_ppm[idx] * ratio[0];
             fputc(old_ppm[idx++], new_ppm);
+
+            gray += old_ppm[idx] * ratio[1];
             fputc(old_ppm[idx++], new_ppm);
+
+            gray += old_ppm[idx] * ratio[2];
             fputc(old_ppm[idx++], new_ppm);
+
             active_row = active_row == width ? 1 : active_row + 1;
+
         } else {
-            gray = 0;
-            for(int i = 0; i < 3; i++, idx++){
+            for(int i = 0; i < 3; ++i, ++idx){
                 current_pixel = 5*old_ppm[idx];
                 current_pixel -= old_ppm[idx-3];
                 current_pixel -= old_ppm[idx-array_width];
@@ -57,23 +66,24 @@ int main(int argc, char *argv[]) {
                 fputc((char)current_pixel, new_ppm);
             }
             ++active_row;
-
-            if(gray < 51){
-                gray_scale[0]++;
-            } else if(gray < 102){
-                gray_scale[1]++;
-            } else if(gray < 153){
-                gray_scale[2]++;
-            } else if(gray < 204){
-                gray_scale[3]++;
-            } else {
-                gray_scale[4]++;
-            }
         }
+        double rounded = round(gray);
+        if(rounded < 51){
+            gray_scale[0]++;
+        } else if(rounded < 102){
+            gray_scale[1]++;
+        } else if(rounded < 153){
+            gray_scale[2]++;
+        } else if(rounded < 204){
+            gray_scale[3]++;
+        } else {
+            gray_scale[4]++;
+        }
+
     }
-    delete[] old_ppm;
-    FILE *histogram = fopen("output.txt", "wb");
-    fprintf(histogram, "%d %d %d %d %d ", gray_scale[0], gray_scale[1], gray_scale[2], gray_scale[3], gray_scale[4]);
+    free(old_ppm);
+    FILE *histogram = fopen(HISTOGRAM, "w");
+    fprintf(histogram, "%d %d %d %d %d", gray_scale[0], gray_scale[1], gray_scale[2], gray_scale[3], gray_scale[4]);
 
     fclose(new_ppm);
     fclose(histogram);
