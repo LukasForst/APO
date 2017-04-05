9f96448b7365ec7bc52002988e15f52c15c75527
forstluk <forstluk@fel.cvut.cz>
2017-04-05 19:15:01 +0200

    working programme, now some optimization

--- hw02/main.cpp
+++ hw02/main.cpp
@@ -11,19 +11,23 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
 
     int width, height, color_max;
-    if (fscanf(old_file, "P6%d %d %d", &width, &height, &color_max) == 0)
+    if (fscanf(old_file, "P6\n%d\n%d\n%d\n", &width, &height, &color_max) == 0){
+        fclose(old_file);
         return EXIT_FAILURE;
+    }
 
     register int current_pixel,
             idx = 0,
             array_width = width * 3,
             size = array_width*height,
-            active_row = 1;
+            active_column = 1;
 
     //allocate and read
     unsigned char *old_ppm = (unsigned char*)malloc(sizeof(unsigned char) * size);
-    if (fread(old_ppm, sizeof(unsigned char), size, old_file) == 0)
+    if (fread(old_ppm, sizeof(unsigned char), (size_t) size, old_file) == 0){
+        fclose(old_file);
         return EXIT_FAILURE;
+    }
     fclose(old_file);
 
     int gray_scale[] = {0,0,0,0,0};
@@ -32,12 +36,38 @@ int main(int argc, char *argv[]) {
     register double gray = 0;
     //write image
     FILE *new_ppm = fopen(OUTPUT, "wb");
-    fprintf(new_ppm, "P6\n%d\n%d\n%d", width, height, color_max);
+    fprintf(new_ppm, "P6\n%d\n%d\n%d\n", width, height, color_max);
 
+    //first row
+    for(;idx < array_width;){
+        gray += old_ppm[idx] * ratio[0];
+        fputc(old_ppm[idx++], new_ppm);
 
+        gray += old_ppm[idx] * ratio[1];
+        fputc(old_ppm[idx++], new_ppm);
 
-    for (;idx != size; gray = 0) {
-        if (active_row == width || active_row == 1 || idx < array_width || idx >= (array_width*height-array_width)) {
+        gray += old_ppm[idx] * ratio[2];
+        fputc(old_ppm[idx++], new_ppm);
+
+        double rounded = round(gray);
+        if(rounded < 51){
+            gray_scale[0]++;
+        } else if(rounded < 102){
+            gray_scale[1]++;
+        } else if(rounded < 153){
+            gray_scale[2]++;
+        } else if(rounded < 204){
+            gray_scale[3]++;
+        } else {
+            gray_scale[4]++;
+        }
+        gray = 0;
+    }
+
+    int fo_end = size-array_width;
+    for (;idx < fo_end;) {
+
+        if(active_column == width){
             gray += old_ppm[idx] * ratio[0];
             fputc(old_ppm[idx++], new_ppm);
 
@@ -47,8 +77,18 @@ int main(int argc, char *argv[]) {
             gray += old_ppm[idx] * ratio[2];
             fputc(old_ppm[idx++], new_ppm);
 
-            active_row = active_row == width ? 1 : active_row + 1;
+            active_column = 1;
+        } else if(active_column == 1){
+            gray += old_ppm[idx] * ratio[0];
+            fputc(old_ppm[idx++], new_ppm);
 
+            gray += old_ppm[idx] * ratio[1];
+            fputc(old_ppm[idx++], new_ppm);
+
+            gray += old_ppm[idx] * ratio[2];
+            fputc(old_ppm[idx++], new_ppm);
+
+            ++active_column;
         } else {
             for(int i = 0; i < 3; ++i, ++idx){
                 current_pixel = 5*old_ppm[idx];
@@ -65,8 +105,9 @@ int main(int argc, char *argv[]) {
 
                 fputc((char)current_pixel, new_ppm);
             }
-            ++active_row;
+            ++active_column;
         }
+
         double rounded = round(gray);
         if(rounded < 51){
             gray_scale[0]++;
@@ -79,13 +120,42 @@ int main(int argc, char *argv[]) {
         } else {
             gray_scale[4]++;
         }
+        gray = 0;
+    }
+
+    //last row
+    for(;idx < size;){
+        gray += old_ppm[idx] * ratio[0];
+        fputc(old_ppm[idx++], new_ppm);
+
+        gray += old_ppm[idx] * ratio[1];
+        fputc(old_ppm[idx++], new_ppm);
+
+        gray += old_ppm[idx] * ratio[2];
+        fputc(old_ppm[idx++], new_ppm);
+
+        active_column++;
+
+        double rounded = round(gray);
+        if(rounded < 51){
+            gray_scale[0]++;
+        } else if(rounded < 102){
+            gray_scale[1]++;
+        } else if(rounded < 153){
+            gray_scale[2]++;
+        } else if(rounded < 204){
+            gray_scale[3]++;
+        } else {
+            gray_scale[4]++;
+        }
+        gray = 0;
 
     }
+
     free(old_ppm);
+    fclose(new_ppm);
     FILE *histogram = fopen(HISTOGRAM, "w");
     fprintf(histogram, "%d %d %d %d %d", gray_scale[0], gray_scale[1], gray_scale[2], gray_scale[3], gray_scale[4]);
-
-    fclose(new_ppm);
     fclose(histogram);
 
     return EXIT_SUCCESS;
