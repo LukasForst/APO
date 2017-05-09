bc3218ee611a3bdddbcbf39deaf9149495705193
Lukas Forst <forstluk@fel.cvut.cz>
2017-05-09 15:44:27 +0200

    animate

--- TP/mzapo/main.c
+++ TP/mzapo/main.c
@@ -5,6 +5,7 @@
 #include <stdint.h>
 #include <time.h>
 #include <unistd.h>
+#include <pthread.h>
 
 #include "mzapo_parlcd.h"
 #include "mzapo_phys.h"
@@ -35,15 +36,38 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    double c_real = -0.7, c_imag = 0.27015, move_x = 0, move_y = 0;
-    color **fractal = generate_julia(WIDTH, HEIGHT, move_x, move_y, c_real, c_imag, 400);
+    double c_real = -0.7, c_imag = 0.27015, move_x = 0, move_y = 0; //-3,3 is max for x, -2,2 is max for y
+    //color **fractal = generate_julia(WIDTH, HEIGHT, move_x, move_y, c_real, c_imag, 400);
+    color **fractal = generate_mandelbrot(WIDTH, HEIGHT, move_x, move_y, 200);
 
-
+/*
     parlcd_write_cmd(parlcd_mem_base, 0x2c);
     for (i = 0; i < WIDTH * HEIGHT; i++) {
         color *c = *(fractal + i);
         uint16_t final_color = convert_struct(c);
         parlcd_write_data(parlcd_mem_base, final_color);
+        free(*(fractal +i));
     }
+    free(fractal);
+*/
+    int depth = 10;
+    while (1) {
+        struct timespec loop_delay = {.tv_sec = 0, .tv_nsec = 200 * 1000 * 1000};
+        clock_nanosleep(CLOCK_MONOTONIC, 0, &loop_delay, NULL);
+
+        fractal = generate_julia(WIDTH, HEIGHT, move_x, move_y, c_real, c_imag, depth);
+        depth = depth >= 2000 ? 10 : depth + 100;
+        move_x = move_x >= 3 ? -3 : move_x + 0.1;
+
+        parlcd_write_cmd(parlcd_mem_base, 0x2c);
+        for (i = 0; i < WIDTH * HEIGHT; i++) {
+            color *c = *(fractal + i);
+            uint16_t final_color = convert_struct(c);
+            parlcd_write_data(parlcd_mem_base, final_color);
+            free(*(fractal + i));
+        }
+        free(fractal);
+    }
+
     return 0;
 }
